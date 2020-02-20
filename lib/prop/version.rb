d85b6b559c316afc7ce237c239b58fe8c418f4b4
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-20 15:20:57 +0900

    Update, version.

--- lib/prop/version.rb
+++ lib/prop/version.rb
@@ -4,7 +4,7 @@ Encoding.default_internal = 'UTF-8'
 Encoding.default_external = 'UTF-8'
 
 module Engine
-  VERSION = '1.0.12.2'.freeze
+  VERSION = '1.0.13'.freeze
 end
 
 __END__
--- test/test_version.rb
+++ test/test_version.rb
@@ -9,7 +9,7 @@ Encoding.default_internal = 'UTF-8'
 class TestVersion < Test::Unit::TestCase
   def setup
     @versions = Engine::VERSION
-    @version = '1.0.12.2'
+    @version = '1.0.13'
   end
 
   def test_new
