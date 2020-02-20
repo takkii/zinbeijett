a62264dcbe24e483a87bc658b1e714df1187aa4e
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-20 14:45:28 +0900

    Update, Encoding external add and kconv delete.

--- lib/prop/version.rb
+++ lib/prop/version.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'kconv'
 Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
 
 module Engine
-  VERSION = '1.0.12.2'.toutf8.freeze
+  VERSION = '1.0.12.2'.freeze
 end
 
 __END__
