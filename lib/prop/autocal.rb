6b881487d91bc4a2709bba013e5bf34e0acd8503
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-02-20 14:52:53 +0900

    Update, no need word delete and kconv delete.

--- lib/prop/autocal.rb
+++ lib/prop/autocal.rb
@@ -1,18 +1,16 @@
 require 'date'
 require 'pycall/import'
 include PyCall::Import
-require 'kconv'
 
 Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
 
 module Auto
   module_function
 
   def view
-    yr = ARGV[1].toutf8
-    mh = ARGV[2].toutf8
-    sys = PyCall.import_module('sys')
-    d = Date.today
+    yr = ARGV[1]
+    mh = ARGV[2]
     pyimport :calendar
     puts (calendar.month(yr.to_i,mh.to_i))
   end
