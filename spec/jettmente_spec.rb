6b40d069833b5b7b0b53353ce0ac93110a1f4e37
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-14 17:15:17 +0900

    Update, no need test delete.

--- spec/jettmente_spec.rb
+++ spec/jettmente_spec.rb
@@ -24,44 +24,9 @@ end
 
 #---Example-2---start---
 
-# Rspec Test Templete case 1.
-class DateTime
-  def test1
-    t = Time.new
-    x = Time.mktime(t.year, t.month, t.day)
-    y = Time.mktime(t.year + 1, 1, 1)
-    ((y - x) / 60 / 60 / 24).round
-  end
-end
-
-describe 'DateTime' do
-  context "when it receives the greet() message" do
-    it "expect say 124日です" do
-      date = DateTime.new.test1
-      expect(date).eql?(124)
-    end
-  end
-end
-
 #---Example-2---end---
 
 #---Example-3---start---
-
-# Rspec Test Templete case 1.
-class DateOver
-  def test1
-    Time.new.strftime('%Y年%m月%d日　%H時%M分%S秒')
-  end
-end
-
-describe 'DateOver' do
-  context "when it receives the greet() message" do
-    it "expect say 2017年8月31日" do
-      dateclock = DateOver.new.test1
-      expect(dateclock).eql?('2017年8月31日')
-    end
-  end
-end
 
 #---Example-3---end---
 
