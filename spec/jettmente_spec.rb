ec9e0251e92c7af9a270634b7a6bbd37d5744de5
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-14 19:35:41 +0900

    Update, comment message.

--- spec/jettmente_spec.rb
+++ spec/jettmente_spec.rb
@@ -4,40 +4,26 @@ require 'spec_helper'
 require 'date'
 require 'himekuri'
 
-RSpec.describe "Zinbeijett" do
+# RSpec Test Start
+RSpec.describe 'Zinbeijett' do
+  before do
+    require 'prop/version'
+  end
 
-before do
-  require 'prop/version'
-end
-
-#---Example-1---start---
-
-describe "Zinbeijett" do
- ver = "1.0.13"
-  it ":expect say '1.0.13' when it receives the greet() message" do
-    expect(Engine::VERSION).eql?(ver)
+  describe 'Zinbeijett' do
+    ver = '1.0.13'
+    it ":expect say '1.0.13' when it receives the greet() message" do
+      expect(Engine::VERSION).eql?(ver)
+    end
   end
 end
 
-#---Example-1---end---
-
-#context "Zinbeijett" do
-#  it ""
-#end
-
-#---Example-2---start---
-
-#---Example-2---end---
-
-#---Example-3---start---
-
-#---Example-3---end---
-
-end
-
+# -- TimeStamp --
 puts ''
 puts ' TIME_STAMP '.center(60, '-')
 puts ''
 puts himekuri_print
 puts ' TIME_STAMP '.center(60, '-')
+# -- TimeStamp --
 
+__END__
