10484ffda2f4fe72a31a970766070c622f9fd445
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-20 18:07:58 +0900

    Update, syntax check.

--- spec/jettmente_spec.rb
+++ spec/jettmente_spec.rb
@@ -17,30 +17,30 @@ RSpec.describe 'Zinbeijett' do
       it { is_expected.to eq('1.0.13') }
     end
 
-  context 'prop/erb_src' do
-    subject { Druby.new.main }
-    it { is_expected.to eq(nil) }
-  end
+    context 'prop/erb_src' do
+      subject { Druby.new.main }
+      it { is_expected.to eq(nil) }
+    end
 
-  context '=~' do
-    subject { 'バナナ ぶどう りんご メロン' =~ (/ぶどう/o) }
-    it { is_expected.to eq(4) }
-  end
+    context '=~' do
+      subject { 'バナナ ぶどう りんご メロン' =~ /ぶどう/o }
+      it { is_expected.to eq(4) }
+    end
 
-  context 'match' do
-    str = 'バナナ ぶどう りんご メロン'.match(/ぶどう/o)
-    subject { 'バナナ ぶどう りんご メロン'.match(/ぶどう/o) }
-    it { is_expected.to eq(str) }
-  end
+    context 'match' do
+      str = 'バナナ ぶどう りんご メロン'.match(/ぶどう/o)
+      subject { 'バナナ ぶどう りんご メロン'.match(/ぶどう/o) }
+      it { is_expected.to eq(str) }
+    end
 
-  context 'prop/calendar' do
-    subject { Calendar.view }
-    it { is_expected.to eq(nil) }
-  end
+    context 'prop/calendar' do
+      subject { Calendar.view }
+      it { is_expected.to eq(nil) }
+    end
 
-  after do
-    GC.start
-  end
+    after do
+      GC.start
+    end
   end
 end
 
