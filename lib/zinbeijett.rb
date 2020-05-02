3c0c8c08c69d88e93ac24f6ee0ac53cc0265453c
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-05-02 18:32:28 +0900

    Update, zinbeijett.rb require load file.

--- lib/zinbeijett.rb
+++ lib/zinbeijett.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'timeout'
+require '../req/require_zinbeij'
 
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
