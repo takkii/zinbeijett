1be4f29eb636dfc23390f9c66eb6b844051a03ac
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-06-06 16:02:12 +0900

    Update, write log.

--- lib/prop/decript.rb
+++ lib/prop/decript.rb
@@ -1,6 +1,11 @@
 require 'openssl'
 require 'io/console'
 
+Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
+
+Dir.mkdir('log', perm = 0o777) unless FileTest.exist?('log')
+
 alg = ARGV[1]
 file = ARGV[2]
 md = ARGV[3]
@@ -15,4 +20,6 @@ salted, salt, encrypted = fp.read.unpack("a8a8a*")
 cipher.pkcs5_keyivgen(passwd, salt, 1, md)
 decrypted = cipher.update(encrypted)
 
-puts decrypted
+File.open('log/cript.txt', 'a:utf-8', perm = 0o777) do |f|
+  f.puts decrypted
+end
