a0644003c6d99f2f95aa18f259d8b5a2e29bf062
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-06-06 16:13:15 +0900

    Update.

--- lib/prop/decript.rb
+++ lib/prop/decript.rb
@@ -1,25 +1,111 @@
+# frozen_string_literal: true
+
 require 'openssl'
-require 'io/console'
 
 Encoding.default_internal = 'UTF-8'
 Encoding.default_external = 'UTF-8'
 
 Dir.mkdir('log', perm = 0o777) unless FileTest.exist?('log')
 
-alg = ARGV[1]
-file = ARGV[2]
-md = ARGV[3]
-fp = open(file,'rb:binary')
-STDERR.print "PASSWORD : "
-passwd = STDIN.noecho{STDIN.gets.chomp}
-STDERR.puts
-
-cipher = OpenSSL::Cipher.new(alg)
-cipher.decrypt
-salted, salt, encrypted = fp.read.unpack("a8a8a*")
-cipher.pkcs5_keyivgen(passwd, salt, 1, md)
-decrypted = cipher.update(encrypted)
-
+# 暗号化クラス
+class String_encrypt
 File.open('log/cript.txt', 'a:utf-8', perm = 0o777) do |f|
-  f.puts(decrypted)
+
+  # 暗号化
+  def use_case_encrypt
+    puts ''
+    # source, password
+    source = 'パスワード暗号化'
+    password = 'passwd'
+    salt = OpenSSL::Random.random_bytes(8)
+    algorithm = 'aes-256-cbc'
+
+    encrypter = OpenSSL::Cipher.new(algorithm).encrypt
+    len = encrypter.key_len + encrypter.iv_len
+    iter = 1024
+    k_i = OpenSSL::PKCS5.pbkdf2_hmac_sha1(password, salt, iter, len)
+    key, iv = k_i.unpack("a#{encrypter.key_len} a#{encrypter.iv_len}")
+
+    encrypter.key = key
+    encrypter.iv = iv
+
+    encrypted = encrypter.update(source) + encrypter.final
+    # encrypted == source
+    f.puts encrypted # 暗号化
+  end
+
+  # 復号化
+  def use_case_decrypt
+    # source, password 暗号化文字列
+    source = 'パスワード復号化'
+    password = 'passwd'
+    salt = OpenSSL::Random.random_bytes(8)
+    algorithm = 'aes-256-cbc'
+
+    encrypter = OpenSSL::Cipher.new(algorithm).encrypt
+    len = encrypter.key_len + encrypter.iv_len
+    iter = 1024
+    k_i = OpenSSL::PKCS5.pbkdf2_hmac_sha1(password, salt, iter, len)
+    key, iv = k_i.unpack("a#{encrypter.key_len} a#{encrypter.iv_len}")
+
+    encrypter.key = key
+    encrypter.iv = iv
+
+    encrypted = encrypter.update(source) + encrypter.final
+    decrypter = OpenSSL::Cipher.new(algorithm).decrypt
+
+    decrypter.key = key
+    decrypter.iv = iv
+
+    decrypted = decrypter.update(encrypted) + decrypter.final
+    # decrypted == source
+    f.puts decrypted # 復号化
+  end
+
+  def use_case_hash
+    # data, key 暗号化対象文字列
+    data = 'パスワード暗号化、ハッシュ'
+    key = 'passwd'
+    digest = OpenSSL::HMAC.new(key, 'sha256')
+    digest.update(data)
+    f.puts digest.hexdigest #暗号化
+  end
+
+  def use_case_public
+    # 鍵対を生成
+    rsa = OpenSSL::PKey::RSA.generate(2048)
+    # 暗号化するデータ
+    data = '*secret data*'
+    # 公開鍵で暗号化
+    encrypt_data = rsa.public_encrypt(data)
+    f.puts rsa.public_key.export # 暗号化
+  end
+
+  def use_case_private
+    # 鍵対を生成
+    rsa = OpenSSL::PKey::RSA.generate(2048)
+    # 暗号化するデータ
+    data = '*secret data*'
+    # 公開鍵で暗号化
+    encrypt_data = rsa.public_encrypt(data)
+    # 秘密鍵で復号化
+    decrypted_data = rsa.private_decrypt(encrypt_data)
+    f.puts decrypted_data # 復号化
+  end
+  end
 end
+
+# aes256
+String_encrypt.new.use_case_encrypt
+puts ''
+# aes256
+String_encrypt.new.use_case_decrypt
+puts ''
+# sha1
+String_encrypt.new.use_case_hash
+puts ''
+# public
+String_encrypt.new.use_case_public
+puts ''
+# private
+String_encrypt.new.use_case_private
