b3210a687428dc3923341e19e04345e0e426e9d4
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-06-29 16:17:43 +0900

    Update, Design.

--- lib/prop/tweet_cmd.rb
+++ lib/prop/tweet_cmd.rb
@@ -10,11 +10,19 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = Settings['access_token_secret']
 end
 
+puts ''
+puts 'Twitter_Timeline'.center(100, '-')
+puts ''
+
 max_id = client.home_timeline.first.id
 client.home_timeline(max_id: max_id,count: 200).each do |tweet|
  puts tweet.user.name
  puts tweet.full_text
  puts "Fav: #{tweet.favorite_count}, Retweet: #{tweet.retweet_count}"
+ puts ''
 end
+
+puts 'Twitter_Timeline'.center(100, '-')
+puts ''
 
 __END__
