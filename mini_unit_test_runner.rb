298611d0b0ab3f03df5a5819c7fa72b5aafd16e5
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-07-19 17:41:44 +0900

    Update, mini-unit-test.

--- mini_unit_test_runner.rb
+++ mini_unit_test_runner.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './req/minitest.rb'
+require './req/mini-unit-test.rb'
 
 # Encoding Setting.
 Encoding.default_internal = 'UTF-8'
@@ -8,44 +8,44 @@ Encoding.default_external = 'UTF-8'
 
 # SubClass test function.
 class MiniTestOne
-  def mini_test
-    Dir[File.dirname(__FILE__) + '/mini_test/*.rb'].sort.select do |file|
-      require file
-    end
-  end
+	def mini_test
+		Dir[File.dirname(__FILE__) + '/mini_unit/*.rb'].sort.select do |file|
+			require file
+		end
+	end
 end
 
 # SubClass current directory
 class MiniFilename
-  def mini_find
-    puts Dir.glob('./mini_test/*.rb')
-  end
+	def mini_find
+		puts Dir.glob('./mini_unit/*.rb')
+	end
 end
 
 # Super Class with refinements methods.
 module MiniFile
-  refine MiniTestOne do
-    def enter
-      mini_test
-    end
-  end
+	refine MiniTestOne do
+		def enter
+			mini_test
+		end
+	end
 end
 
 # Super Class feeting refinements mehtod.
 module MiniFileN
-  refine MiniFilename do
-    def anker
-      mini_find
-    end
-  end
+	refine MiniFilename do
+		def anker
+			mini_find
+		end
+	end
 end
 
 # Console Output list.
-puts ''
 puts ' TimeStamp '.center(60, '-')
 puts ''
-print '現在時刻 : '
-Himekuri.new.cal
+HimekuriClass.new.himekuri_print
+puts ''
+HimekuriClass.new.reiwa_print
 puts ''
 puts ' TimeStamp '.center(60, '-')
 puts ''
