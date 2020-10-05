f01acda4f90339fc16555d1da7a424722234063c
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-10-05 17:26:04 +0900

    Update, rubocop syntax check.

--- mini_test_runner.rb
+++ mini_test_runner.rb
@@ -44,22 +44,33 @@ end
 class Nyanco
   def self.big
     # Console Output list.
-    puts ''; puts ' TimeStamp '.center(60, '-'); puts ''; Himekuri.new.cal; puts ''
-    puts '日めくり2、バージョン : ' + Himekuri2::VERSION; puts ''
-    puts ' TimeStamp '.center(60, '-'); puts ''
-    puts ' minitest file load '.center(80, '~'); puts ''
+    puts ''
+    puts ' TimeStamp '.center(60, '-')
+    puts ''
+    Himekuri.new.cal
+    puts ''
+    puts '日めくり2、バージョン : ' + Himekuri2::VERSION
+    puts ''
+    puts ' TimeStamp '.center(60, '-')
+    puts ''
+    puts ' minitest file load '.center(80, '~')
+    puts ''
   end
 end
 
 Nyanco.big
 
 using MiniFileN
-MiniFilename.new.mini_find; puts ''
+MiniFilename.new.mini_find
+puts ''
 
-puts ' minitest file load '.center(80, '~'); puts ''
-puts ' mini_test in filename list '.center(80, '~'); puts ''
+puts ' minitest file load '.center(80, '~')
+puts ''
+puts ' mini_test in filename list '.center(80, '~')
+puts ''
 
 using MiniFile
-MiniTestOne.new.mini_test; puts ''
+MiniTestOne.new.mini_test
+puts ''
 
 __END__
