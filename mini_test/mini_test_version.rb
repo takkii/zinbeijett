98987f2c378a3bd8084a7b3e97d1126911195409
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-11-09 17:38:27 +0900

    Update, minitest.

--- mini_test/mini_test_version.rb
+++ mini_test/mini_test_version.rb
@@ -7,7 +7,11 @@ require_relative File.join(File.dirname(__FILE__), '../lib/prop/version.rb')
 # Mini_test file load.
 class VersionTest < Minitest::Test
   def test_version
-    assert_equal EngineLeft::VERSION, '1.1.8'
+    @v1 = EngineLeft::VERSION
+    @v2 = '2.0.0'
+
+    refute_equal(@v1, @v2)
+    assert_operator(@v1, :<, @v2)
   end
 end
 
