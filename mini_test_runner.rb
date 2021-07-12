8b299af06661849fd388523e7c4e21bc66ae1fca
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-07-12 10:39:53 +0900

    Delete, no need blank delete.

--- mini_test_runner.rb
+++ mini_test_runner.rb
@@ -60,18 +60,14 @@ end
 
 begin
   puts ' mini_test in filename list '.center(80, '~')
-
   Nyanco.big
-
   using MiniFileN
   MiniFilename.new.mini_find
   puts ''
-
   puts ' minitest file load '.center(80, '~')
   puts ''
   puts ' mini_test in filename list '.center(80, '~')
   puts ''
-
   using MiniFile
   MiniTestOne.new.mini_test
   puts ''
