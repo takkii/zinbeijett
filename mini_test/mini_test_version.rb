a26f525de7487225bad1fdf4b185f6c388c0c007
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-08-14 08:28:25 +0900

    Update, rubocop check.

--- mini_test/mini_test_version.rb
+++ mini_test/mini_test_version.rb
@@ -12,7 +12,7 @@ class VersionTest < Minitest::Test
   def test_version
     @v1 = EngineLeft::VERSION
     @v2 = '2.0.0'
-    
+
     refute_equal(@v1, @v2)
     assert_operator(@v1, :<, @v2)
   end
