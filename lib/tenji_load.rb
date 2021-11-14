07b15167a675c05d098c944c56b6241d977b2fc5
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-11-14 11:38:09 +0900

    In the middle, Tenji to Romaji convert.

---
+++ lib/tenji_load.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+Encoding.default_internal = 'UTF-8'
+Encoding.default_external = 'UTF-8'
+
+class TenjiMaker
+  def self.to_tenji_a
+    one = ARGV[0]
+
+    File.open(one) do |f|
+      while (str1 = f.read)
+        str2 = str1.chomp!
+        alpha = str2.to_s
+
+        tenji_a = <<~EOS.chomp
+
+          o-
+          --
+          --
+
+        EOS
+
+        tenji_i = <<~EOS.chomp
+
+          o-
+          o-
+          --
+
+        EOS
+
+        tenji_a_s = tenji_a.to_s
+        tenji_i_s = tenji_i.to_s
+
+        if alpha.match?(tenji_a_s)
+        puts ''
+        puts 'A'
+        puts ''
+        exit!
+
+        elsif alpha.match?(tenji_i_s)
+        puts ''
+        puts 'I'
+        puts ''
+        exit!
+
+        end
+      end
+    end
+  end
+end
+
+TenjiMaker.to_tenji_a
+
+GC.compact
+
+__END__
