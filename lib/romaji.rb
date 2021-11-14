6cb605813c329b33859016eda0c55c94c9587d01
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-11-14 18:55:18 +0900

    Update, sa ~ so.

--- lib/romaji.rb
+++ lib/romaji.rb
@@ -12,6 +12,7 @@ class TenjiMaker
         str2 = str1.chomp!
         alpha = str2.to_s
 
+        # A ~ O
         tenji_a = <<~EOS.chomp
 
           o-
@@ -52,6 +53,7 @@ class TenjiMaker
 
         EOS
 
+        # KA ~ KO
         tenji_ka = <<~EOS.chomp
 
           o-
@@ -92,6 +94,46 @@ class TenjiMaker
 
         EOS
 
+        tenji_sa = <<~EOS.chomp
+
+          o-
+          -o
+          -o
+
+        EOS
+
+        tenji_si = <<~EOS.chomp
+
+          o-
+          oo
+          -o
+
+        EOS
+
+        tenji_su = <<~EOS.chomp
+
+          oo
+          -o
+          -o
+
+        EOS
+
+        tenji_se = <<~EOS.chomp
+
+          oo
+          oo
+          -o
+
+        EOS
+
+        tenji_so = <<~EOS.chomp
+
+          -o
+          oo
+          -o
+
+        EOS
+
         # A ~ O
         tenji_a_s = tenji_a.to_s
         tenji_i_s = tenji_i.to_s
@@ -99,13 +141,21 @@ class TenjiMaker
         tenji_e_s = tenji_e.to_s
         tenji_o_s = tenji_o.to_s
 
-        # KA KO
+        # KA ~ KO
         tenji_ka_s = tenji_ka.to_s
         tenji_ki_s = tenji_ki.to_s
         tenji_ku_s = tenji_ku.to_s
         tenji_ke_s = tenji_ke.to_s
         tenji_ko_s = tenji_ko.to_s
 
+        # SA ~ SO
+        tenji_sa_s = tenji_sa.to_s
+        tenji_si_s = tenji_si.to_s
+        tenji_su_s = tenji_su.to_s
+        tenji_se_s = tenji_se.to_s
+        tenji_so_s = tenji_so.to_s
+
+        # A ~ O
         if alpha.match?(tenji_a_s)
           puts ''
           puts 'A'
@@ -136,6 +186,7 @@ class TenjiMaker
           puts ''
           exit!
 
+        # KA ~ KO
         elsif alpha.match?(tenji_ka_s)
           puts ''
           puts 'KA'
@@ -163,6 +214,37 @@ class TenjiMaker
         elsif alpha.match?(tenji_ko_s)
           puts ''
           puts 'KO'
+          puts ''
+          exit!
+
+        # SA ~ SO
+        elsif alpha.match?(tenji_ko_s)
+          puts ''
+          puts 'SA'
+          puts ''
+          exit!
+
+        elsif alpha.match?(tenji_ko_s)
+          puts ''
+          puts 'SI'
+          puts ''
+          exit!
+
+        elsif alpha.match?(tenji_ko_s)
+          puts ''
+          puts 'SU'
+          puts ''
+          exit!
+
+        elsif alpha.match?(tenji_ko_s)
+          puts ''
+          puts 'SE'
+          puts ''
+          exit!
+
+        elsif alpha.match?(tenji_ko_s)
+          puts ''
+          puts 'SO'
           puts ''
           exit!
 
