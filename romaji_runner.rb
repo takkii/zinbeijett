98912ef39d979f0e6ba5cfa8550e6cccb89b9f0d
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-01-15 16:58:03 +0900

    Update.

--- romaji_runner.rb
+++ romaji_runner.rb
@@ -109,8 +109,9 @@ rescue StandardError => e
   puts e.backtrace
   encodig_style.tanraku_exit
 rescue Interrupt
-  # false → default true
+  # [now] false → [default] true
   # puts Thread.current.report_on_exception
+  puts ""
   puts "Romaji Runner is Stopped."
   puts ""
 ensure
