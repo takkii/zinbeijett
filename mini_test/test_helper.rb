2e5948b78a8927a4e840b779b96ed421389b53b1
Takayuki Kamiyama <karuma.reason@gmail.com>
2023-06-14 17:48:34 +0900

    Delete.

--- mini_test/test_helper.rb
+++ mini_test/test_helper.rb
@@ -1,10 +1,3 @@
 ﻿# frozen_string_literal: true
 
 require 'minitest/autorun'
-require 'minitest/reporters'
-
-Minitest::Reporters.use!(
-  Minitest::Reporters::SpecReporter.new,
-  ENV,
-  Minitest.backtrace_filter
-)
