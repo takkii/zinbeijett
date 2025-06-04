2831bd023b4c61fb436e32927d94e53cd60899e7
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-06-04 15:11:56 +0900

    Update.

--- lib/tanraku.rb
+++ lib/tanraku.rb
@@ -1,5 +1,6 @@
-﻿# frozen_string_literal: true
+# frozen_string_literal: true
 
+# Can add it to the object method chain
 class Object
   def tanraku
     m = Mutex.new
@@ -59,9 +60,76 @@ class Object
 end
 
 module Tanraku
-  VERSION = '1.1.1'
+  VERSION = '1.1.2'
 end
 
-GC.compact
+# Can be used as a function
+module Short_Circuit
+  module_function
+
+  def tanraku
+    m = Mutex.new
+
+    begin
+      m.lock
+      raise
+    rescue StandardError => e
+      puts e.backtrace
+      print 'Tanraku Method Exception '
+    ensure
+      puts '--> next class search...'
+      m.unlock
+    end
+  end
+
+  def tanraku_exit
+    m2 = Mutex.new
+
+    begin
+      m2.lock
+      raise
+    rescue StandardError => ef
+      puts ef.backtrace
+    ensure
+      puts '--> Tanraku_Exit Method Exception'
+      m2.unlock
+      exit!
+    end
+  end
+
+  def tanraku_execute
+    raise
+  rescue StandardError => e
+    puts "#{e.class} : #{e.message}"
+    puts e.backtrace
+  ensure
+    puts '--> Tanraku_Execute Method Exception'
+    exit!
+  end
+
+  def tanraku_log
+    Dir.mkdir('log', 0o666) unless FileTest.exist?('log')
+    File.open('log/logs.txt', 'a:utf-8') do |m2|
+      exee = Exception.new('User Exception is raise message.')
+      m2.puts exee.message
+      raise
+    rescue StandardError => e
+      File.open('log/logs.txt', 'a:utf-8') do |m|
+        m.puts e.backtrace.to_s
+        m.puts "#{e.class} : #{e.message}"
+      end
+    ensure
+      puts 'log write (log/logs.txt).'
+    end
+  end
+end
+
+begin
+  include Short_Circuit
+rescue
+  tanraku_execute
+ensure
+  GC.compact
+end
 
 __END__
