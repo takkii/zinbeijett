0da5280b104bdcd92139feccc19af1f1b449af98
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-06 07:05:31 +0900

    Create.

---
+++ req/himekuri.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# --------------------------------------
+require 'date'
+require 'open3'
+require 'time'
+require 'version'
+require 'zella'
+# --------------------------------------
