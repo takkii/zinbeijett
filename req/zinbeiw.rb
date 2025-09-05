de4898bdc7f39912968050bfba3ce56483b1819b
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-06 08:47:30 +0900

    Create.

---
+++ req/zinbeiw.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# -------------------------------------
+require 'date'
+require 'open3'
+require 'readline'
+require 'pycall/import'
+include PyCall::Import
+# -------------------------------------
