89a2dda074078efa8ede820541d2a4d7ba6e9b84
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-06 05:55:29 +0900

    Update.

--- lib/version.rb
+++ lib/version.rb
@@ -11,6 +11,15 @@ module EngineLeft
   VERSION = "#{zinbeijett_version}-#{build_day}".freeze
   #VERSION = "#{zinbeijett_version}"
 end
+
+# bmi_version = x.x.x-xxxx.xx.xx
+module BMI_CLI
+  bmi_version = '1.0.3.2'
+  t = Date.today
+  build_day = t.strftime('%Y.%m.%d')
+  VERSION = "#{bmi_version}-#{build_day}".freeze
+  #VERSION = "#{bmi_version}"
+end
 rescue StandardError => e
   puts e.backtrace
 ensure
