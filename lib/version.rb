843d77b42cb313919178e33a4615219359f235c5
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-14 17:02:58 +0900

    Update.

--- lib/version.rb
+++ lib/version.rb
@@ -5,7 +5,7 @@ require 'date'
 begin
 module EngineLeft
   # zinbeijett_version = x.x.x
-  zinbeijett_version = '4.0.3.7.1'
+  zinbeijett_version = '4.0.3.8'
   VERSION = "#{zinbeijett_version}"
 end
 
