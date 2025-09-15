85b902ed2bcb6bf275a5c6ec14e3b9edb2865a0b
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-15 13:00:01 +0900

    Delete.

--- lib/version.rb
+++ lib/version.rb
@@ -9,29 +9,6 @@ module EngineLeft
   VERSION = "#{zinbeijett_version}"
 end
 
-# bmi_version = x.x.x-xxxx.xx.xx
-module BMI_CLI
-  bmi_version = '1.0.3.2'
-  t = Date.today
-  build_day = t.strftime('%Y.%m.%d')
-  BMI_VERSION = "#{bmi_version}-#{build_day}".freeze
-end
-
-# himekuri_version = x.x.x-xxxx.xx.xx
-module Himekuri
-  himekuri_version = '0.2.5'
-  t = Date.today
-  build_day = t.strftime('%Y.%m.%d')
-  HIMEKURI_VERSION = "#{himekuri_version}-#{build_day}".freeze
-end
-
-# nyasocom_frame version = x.x.x-xxxx.xx.xx
-module Frame
-  frame_version = '1.2.3'
-  t = Date.today
-  build_day = t.strftime('%Y.%m.%d')
-  FRAME_VERSION = "#{frame_version}-#{build_day}".freeze
-end
 rescue StandardError => e
   puts e.backtrace
 ensure
