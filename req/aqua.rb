c6e5e7c8e42e16eeacb379d65d161a4cb4c99fd2
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-15 13:37:29 +0900

    Update.

---
+++ req/aqua.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# --------------------------------------
+require 'bmi'
+require 'tanraku'
+require 'timeout'
+# --------------------------------------
+encoding_style
+# --------------------------------------
