96c4f633b5b528a41b060e9ae9cab23b00540747
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-15 12:35:38 +0900

    Rename.

--- req/nyasocom_frame.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# --------------------------------------
-require 'bmi'
-require 'date'
-require 'fileutils'
-require 'version'
-# --------------------------------------
