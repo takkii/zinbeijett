ca79e03909ea4e88a041467fce3c045e4844aa01
Takayuki Kamiyama <karuma.reason@gmail.com>
2025-09-15 12:57:18 +0900

    Update.

--- lib/frame.rb
+++ lib/frame.rb
@@ -3,7 +3,7 @@
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require "#{File.dirname(__FILE__)}/../req/frame"
+require "#{File.dirname(__FILE__)}/../req/frame_version"
 
 # Heat module
 module Heart
